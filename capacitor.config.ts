05c3985b98acb67462c4b3f28c225a2cff2905fa
gormlabenz <gorm-labenz@hotmail.com>
2022-04-25 13:44:10 +0200

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.gormlabenz.capmap',
+  appName: 'Capmap',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
