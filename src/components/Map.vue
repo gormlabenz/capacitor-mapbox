6b74a4090c8ae42a8156350641f3e5ee93378907
gormlabenz <gorm-labenz@hotmail.com>
2022-04-25 14:20:21 +0200

    map is rotating

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,30 @@
 <script setup>
 import mapbox from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 const mapContainer = ref(null)
 mapbox.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+
+function rotateCamera(map, timestamp) {
+  map.rotateTo((timestamp / 100) % 360, {
+    duration: 0,
+  })
+  requestAnimationFrame((timestamp) => rotateCamera(map, timestamp))
+}
 
 onMounted(() => {
   const map = new mapbox.Map({
     container: mapContainer.value, // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9, // starting zoom
   })
+  map.on('load', (event) => {
+    rotateCamera(event.target, 0)
+  })
 })
 </script>
+
 <template>
   <div class="w-screen h-screen overflow-hidden">
     <div ref="mapContainer" class="w-full h-full"></div>
